9ca49d31b6dd17c51e284052ec62ac30dc1f04b4
Kim McCallum <kchaase@gmail.com>
2020-04-15 12:50:53 -0600

    managed to seed the database

--- Seeds/seed-db.js
+++ Seeds/seed-db.js
@@ -8,14 +8,21 @@ let db = knex({
     client:'pg',
     connection: DATABASE_URL,
 })
+//   // TO DO: 
+//   // Figure out how to close the connection - the script is hanging on for some reason
+//   // Add error handling
+//   // Add robust logging in addition to error handling so that we can go back and check/troubleshoot
+//   // What happened, error message, etc. What if your Cron job crashes?
+//   .then(() => {
+//     db.destroy();
+//     console.log('Hoorah!')
+//   })
 
-//// WHEN I DO IT THIS WAY POPULATING THE FIRST TWO TABLES WORKS BUT NOT THE THIRD (observations)
-// FIRST insert the study id into the species_study table
 db('species_study').insert(
     {study_id: eagles.individuals[0].study_id, individual_taxon_canonical_name: eagles.individuals[0].individual_taxon_canonical_name}
-).then(() => {
+  ).then(() => {
     // SECOND map over the individuals and put them into the individuals table
-    eagles.individuals.map(ind => {
+    eagles.individuals.forEach(ind => {
         //this works
         db('individuals').insert({
             individual_local_identifier: ind.individual_local_identifier,
@@ -23,58 +30,21 @@ db('species_study').insert(
         })
         // THIRD Within that individual, map over locations and put data into observations
         .then(()=> {
-            // Not working - ind is not defined in here??? 
-            ind.locations.map(obs => {
+              ind.locations.forEach(obs => {
                 db('observations').insert({
                     individual_id: ind.individual_local_identifier,
                     time_stamp: obs.timestamp,
                     location_long: obs.location_long,
                     location_lat: obs.location_lat, 
                     heading: obs.heading,
-                    height_raw: obs.height_raw, 
                     ground_speed: obs.ground_speed
-                })
+                }).then(() => {console.log(`We inserted ${JSON.stringify(obs)}`)})
             })
         })
     })
-})
+  })
 
 
-//// WHEN I DO IT THIS WAY NOTHING WORKS
-//First 
-// db('species_study').insert(
-//     {study_id: eagles.individuals[0].study_id, individual_taxon_canonical_name: eagles.individuals[0].individual_taxon_canonical_name}
-// ).then(() => {
-//     // SECOND map over the individuals and put them into the individuals table
-//     eagles.individuals.map(ind => {
-//         //this works
-//         db('individuals').insert({
-//             individual_local_identifier: ind.individual_local_identifier,
-//             study_id: ind.study_id
-//         })
-//     })
-// }).then(() => {
-//     console.log('done with species_study and individuals')
-// });
-
-// // THIRD Within that individual, map over locations and put data into observations
-// eagles.individuals.map(ind => {
-//     ind.locations.map((obs) => {
-//         db('observations').insert({
-//             individual_id: ind.individual_local_identifier,
-//             time_stamp: obs.timestamp,
-//             location_long: obs.location_long,
-//             location_lat: obs.location_lat, 
-//             heading: obs.heading,
-//             ground_speed: obs.ground_speed
-//         }).then() 
-//         // console.log(`Individual: ${ind.individual_local_identifier}`)
-//         // console.log(`Some timestamp: ${typeof(obs.timestamp)}`)
-//         // console.log(`Some ground_speed: ${typeof(obs.ground_speed)}`)
-//         // console.log(`Some location_lat: ${typeof(obs.location_lat)}`)
-//     })
-// })
-
-
+  
 
        
