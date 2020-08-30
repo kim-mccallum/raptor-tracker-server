2170b17a4f76be07c99c4a98585517ecb6d5ba6a
Kim McCallum <kchaase@gmail.com>
2020-08-30 15:59:08 -0600

    created new Heroku app

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
+
+console.log(process.env.DATABASE_URL);
 
 module.exports = {
-  "migrationsDirectory": "migrations",
-  "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test') 
-    ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
-  "ssl":!!process.env.SSL,
-}
+  migrationsDirectory: "migrations",
+  driver: "pg",
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  ssl: !!process.env.SSL,
+};
